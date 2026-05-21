13ff05116c13be9a779adc87c683e8ea0991db10
Deni Supriyatna <denisupriyatna01@gmail.com>
2026-05-21 23:09:58 +0700

    Add : cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const db = require("./db");
@@ -9,6 +10,7 @@ const db = require("./db");
 const app = express();
 const port = 3001;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(
